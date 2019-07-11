a2ccb0ae33f0df60905983590c4192ccacb91d6f
Faithfinder <faithfinder@yandex.ru>
2019-07-11 18:46:48 -0400

    Customizable router history

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 
 import Header from "./header/Header";
 import AboutMe from "./aboutMe/AboutMe";
@@ -8,11 +8,12 @@ import ExperienceList from "./experience/ExperienceList";
 import ProjectList from "./portfolio/ProjectList";
 import Contacts from "./contacts/Contacts";
 import paths from "./header/paths";
+import history from "../history";
 
 const App = () => {
   return (
     <Container>
-      <Router>
+      <Router history={history}>
         <Header />
         <Switch>
           <Route path={paths.root} exact component={AboutMe} />
