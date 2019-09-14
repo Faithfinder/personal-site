1b4441dd0d171e73123a2dc03feb8e6e6fa1daa6
Faithfinder <faithfinder@yandex.ru>
2019-09-14 17:30:30 -0400

    Language persistance

--- client/src/reducers/languageReducer.js
+++ client/src/reducers/languageReducer.js
@@ -1,11 +1,13 @@
 import { CHANGE_LANGUAGE } from "../actions/types";
 import languages from "../i18n/languages";
 
-const initialState = languages.en.value;
+const initialState =
+  localStorage.getItem("currentLanguage") || languages.en.value;
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
     case CHANGE_LANGUAGE:
+      localStorage.setItem("currentLanguage", payload);
       return payload;
 
     default:
