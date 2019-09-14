59795d6d1a25f2efd8d3007cbfd174dd3fcf31f8
Faithfinder <faithfinder@yandex.ru>
2019-09-14 15:47:42 -0400

    Bakground

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Container, Segment } from "semantic-ui-react";
 import { Router, Route, Switch } from "react-router-dom";
 
+import "./App.css";
 import Header from "./header/Header";
 import AboutMe from "./aboutMe/AboutMe";
 import ExperienceList from "./experience/ExperienceList";
